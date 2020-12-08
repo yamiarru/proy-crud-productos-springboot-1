68eb8df546c17110c332c19b422d41981caa25d3
Yamila Arru <yamiarru@gmail.com>
2020-12-08 20:34:22 -0300

    Se sube arquitectura base del proyecto

---
+++ src/test/java/com/springcodoacodoarru/ProyCrudProductosSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcodoacodoarru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyCrudProductosSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
